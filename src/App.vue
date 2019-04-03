b8c62202e33afd8aef12425526a2d761ed6b2d88
王骁 <403944557@qq.com>
2019-04-03 13:15:35 +0800

    add Animate.css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="app">
     <router-view />
+    <!--<transition enter-active-class="animated slideInLeft"-->
+                <!--leave-active-class="animated slideOutRight">-->
+      <!--<router-view/>-->
+    <!--</transition>-->
   </div>
 </template>
+
+<!--<style scoped>-->
+  <!--.animated {-->
+    <!-- -webkit-animation-duration: .5s;-->
+    <!--animation-duration: .5s;-->
+  <!--}-->
+  <!--.slideInLeft {-->
+    <!--width: 100%;-->
+    <!--position: absolute;-->
+  <!--}-->
+  <!--.slideOutRight {-->
+    <!--width: 100%;-->
+    <!--position: absolute;-->
+  <!--}-->
+<!--</style>-->
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import axios from "./axios.js";
 import ElementUI from "element-ui";
 import "./libs/util.js";
 import "element-ui/lib/theme-chalk/index.css";
+// import "animate.css"
 
 if (process.env.NODE_ENV === "development") {
   require("./mock/index");
