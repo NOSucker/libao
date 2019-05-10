1da44244ff31bb021369c0a7c5d5fa6dbbaa196c
王骁 <403944557@qq.com>
2019-05-10 11:07:21 +0800

    add driective "form-track"

---
+++ src/directive/form-track/formTrack.js
@@ -0,0 +1,83 @@
+export default {
+  bind: function(el, binding, vnode) {
+    vnode.componentInstance.trackValue = binding.value;
+    vnode.componentInstance.$on("el.form.addField", field => {
+      field.trackLabel = null;
+      field.$on("el.form.change", newVal => {
+        let oldVal = vnode.componentInstance.trackValue;
+        if (oldVal) {
+          field.prop.split(".").forEach(p => {
+            if (oldVal) {
+              oldVal = oldVal[p];
+            }
+          });
+
+          if (typeof newVal === "object") {
+            newVal = JSON.stringify(newVal);
+          }
+          if (typeof oldVal === "object") {
+            oldVal = JSON.stringify(oldVal);
+          }
+
+          if (
+            newVal !== oldVal &&
+            !(!newVal && !oldVal) &&
+            field.validateState !== "error"
+          ) {
+            if (
+              field.$slots.default.length > 0 &&
+              field.$slots.default[0].componentInstance.$options
+                ._componentTag === "el-select"
+            ) {
+              oldVal =
+                field.$slots.default[0].componentInstance.getOption(oldVal)
+                  .label || oldVal;
+              newVal =
+                field.$slots.default[0].componentInstance.getOption(newVal)
+                  .label || newVal;
+            }
+            if (!field.trackLabel) {
+              field.trackLabel = document.createElement("div");
+              field.trackLabel.className =
+                "el-form-item__track el-form-item__error";
+              field.trackLabel.innerHTML = `原值：${oldVal}`;
+              field.$el
+                .querySelector(".el-form-item__content")
+                .appendChild(field.trackLabel);
+              field.$el.classList.add("is-track");
+            }
+          } else {
+            if (field.trackLabel) {
+              field.$el
+                .querySelector(".el-form-item__content")
+                .removeChild(field.trackLabel);
+              field.$el.classList.remove("is-track");
+              field.trackLabel = null;
+            }
+          }
+          vnode.componentInstance.$emit("form-track", {
+            prop: field.prop,
+            label: field.label,
+            oldValue: oldVal,
+            newValue: newVal
+          });
+        }
+      });
+
+      field.$watch("validateState", valid => {
+        if (field.trackLabel) {
+          if (valid === "error") {
+            field.trackLabel.style = "display:none";
+            field.$el.classList.remove("is-track");
+          } else {
+            field.trackLabel.style = "";
+            field.$el.classList.add("is-track");
+          }
+        }
+      });
+    });
+  },
+  update: function(el, binding, vnode) {
+    vnode.componentInstance.trackValue = binding.value;
+  }
+};
