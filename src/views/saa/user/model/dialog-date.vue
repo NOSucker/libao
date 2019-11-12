cff013d74a8571881262c2b3961497ad5da2cb70
chenjunlin <chenjl@sinosoft.com.cn>
2019-11-12 11:09:07 +0800

    修改日期图标显示

--- src/views/saa/user/model/dialog-date.vue
+++ src/views/saa/user/model/dialog-date.vue
@@ -8,7 +8,6 @@
           </el-form-item>
         </el-row>
       </el-form>
-      >
       <span slot="footer">
         <el-button type="primary" @click="submitForm">确认</el-button>
       </span>
@@ -17,58 +16,58 @@
 </template>
 
 <script>
-export default {
-  name: "DialogDate",
-  props: {
-    value: Boolean,
-    userRoleId: String
-  },
-  data() {
-    return {
-      submitLoading: false,
-      dateForm: {
-        userRoleId: null,
-        overTime: null
-      },
-      editDialogTitle: ""
-    };
-  },
-  computed: {
-    showDialogDate: {
-      set(val) {
-        this.$emit("input", val);
-      },
-      get() {
-        return this.value;
+  export default {
+    name: "DialogDate",
+    props: {
+      value: Boolean,
+      userRoleId: String
+    },
+    data() {
+      return {
+        submitLoading: false,
+        dateForm: {
+          userRoleId: null,
+          overTime: null
+        },
+        editDialogTitle: ""
+      };
+    },
+    computed: {
+      showDialogDate: {
+        set(val) {
+          this.$emit("input", val);
+        },
+        get() {
+          return this.value;
+        }
+      }
+    },
+    methods: {
+      submitForm() {
+        this.dateForm.userRoleId = this.userRoleId;
+        console.log(5120124554, this.dateForm);
+
+        let dateParams = {
+          requestUrl: this.$axios.config.user.baseURL + this.$axios.config.user.setUserRoleDate,
+          requestType: "POST",
+          requestBody: JSON.stringify(this.dateForm)
+        };
+        this.$axios
+          .post(this.$axios.config.service.baseURL + this.$axios.config.service.transitInterface, dateParams)
+          .then(response => {
+            console.log(11111111, response);
+            if (JSON.parse(response.data.responseStr).errorCode == 200) {
+              this.$message.success("提升成功！");
+              this.$emit("input", false);
+              this.$emit("role-date-close");
+            } else {
+              this.$message.error(JSON.parse(response.data.responseStr).msg);
+            }
+          })
+          .finally(() => {});
       }
     }
-  },
-  methods: {
-    submitForm() {
-      this.dateForm.userRoleId = this.userRoleId;
-      console.log(5120124554, this.dateForm);
-
-      let dateParams = {
-        requestUrl: this.$axios.config.user.baseURL + this.$axios.config.user.setUserRoleDate,
-        requestType: "POST",
-        requestBody: JSON.stringify(this.dateForm)
-      };
-      this.$axios
-        .post(this.$axios.config.service.baseURL + this.$axios.config.service.transitInterface, dateParams)
-        .then(response => {
-          console.log(11111111, response);
-          if (JSON.parse(response.data.responseStr).errorCode == 200) {
-            this.$message.success("提升成功！");
-            this.$emit("input", false);
-            this.$emit("role-date-close");
-          } else {
-            this.$message.error(JSON.parse(response.data.responseStr).msg);
-          }
-        })
-        .finally(() => {});
-    }
-  }
-};
+  };
 </script>
 
 <style scoped></style>
